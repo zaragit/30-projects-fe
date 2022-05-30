20d355c30ffb41622ee7bee437c4286d3df98fa2
zaradev <p080931@gmail.com>
2022-05-30 19:05:08 +0900

    가상 키보드 강좌 학습

---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    open: true,
+    watchFiles: "index.html",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "keyboard",
+      template: "./index.html",
+      inject: "body",
+      favicon: "./favicon.ico",
+    }),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+  ],
+  optimization: {
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerWebpackPlugin()],
+  },
+};
